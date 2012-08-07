c08c21e76bef452e4a09a9aec9dcbc46d513334c
Richard Bausek <office@artibella.com>
2012-08-07 22:47:34 +0200

    added content pages and flexslider

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,10 @@
-  $(document).ready(function(){
-    $(".video").fitVids();
+$(document).ready(function(){
+  $(".video").fitVids();
+});
+
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "fade",
+    controlNav: false
   });
-
-
+});
